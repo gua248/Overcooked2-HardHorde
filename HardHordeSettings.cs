c5c9cb850f7023959155c897781d1486091c4e61
GUA <gua@pku.edu.cn>
2023-12-25 23:16:35 +0800

    change GetChild() to Find()

--- HardHordeSettings.cs
+++ HardHordeSettings.cs
@@ -17,10 +17,10 @@ namespace OC2HardHorde
         private static void AddHardHordeSettingsUI()
         {
             if (modSettingsMenu == null || hardHordeOption != null) return;
-            GameObject hardHordeOptionObj = GameObject.Instantiate(gameSettingsMenu.transform.GetChild(0).Find("ContentPC").GetChild(1).GetChild(0).GetChild(3).gameObject);
+            GameObject hardHordeOptionObj = GameObject.Instantiate(gameSettingsMenu.transform.Find("SettingsBody/ContentPC/Viewport/Content/Windowed").gameObject);
             hardHordeOptionObj.name = "HardHorde";
-            hardHordeOptionObj.transform.SetParent(modSettingsMenu.transform.GetChild(0).Find("ContentPC").GetChild(1).GetChild(0), false);
-            T17Text text = hardHordeOptionObj.transform.GetChild(0).GetComponent<T17Text>();
+            hardHordeOptionObj.transform.SetParent(modSettingsMenu.transform.Find("SettingsBody/ContentPC/Viewport/Content"), false);
+            T17Text text = hardHordeOptionObj.transform.Find("Title").GetComponent<T17Text>();
             if (Localization.GetLanguage() == SupportedLanguages.Chinese)
             {
                 text.text = "困难敌群";
@@ -43,23 +43,24 @@ namespace OC2HardHorde
             GameObject root = GameObject.Find("FrontendRootMenu");
             if (root == null) return;
             FrontendRootMenu frontendRootMenu = root.GetComponent<FrontendRootMenu>();
-            gameSettingsMenu = root.transform.GetChild(1).GetChild(0).Find("GameOptions").GetComponent<FrontendOptionsMenu>();
-            modSettingsMenu = root.transform.GetChild(1).GetChild(0).Find("ModOptions")?.GetComponent<FrontendOptionsMenu>();
+            gameSettingsMenu = root.transform.Find("FixedAspectRootCanvas/ScreenSpaceCanvas/GameOptions").GetComponent<FrontendOptionsMenu>();
+            modSettingsMenu = root.transform.Find("FixedAspectRootCanvas/ScreenSpaceCanvas/ModOptions")?.GetComponent<FrontendOptionsMenu>();
             if (modSettingsMenu != null) return;
 
-            Transform settingsTab = root.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(4);
-            float dh = settingsTab.GetChild(0).GetComponent<RectTransform>().rect.height;
+            Transform settingsTab = root.transform.Find("WorldSpaceCanvas/Safezone_Rootmenu/RootTab/SettingsOptions");
+            float dh = settingsTab.Find("GameSettings").GetComponent<RectTransform>().rect.height;
             RectTransform rect = settingsTab.GetComponent<RectTransform>();
             float h = rect.rect.height;
             rect.offsetMin += new Vector2(0, -dh);
             float py = rect.pivot.y;
             rect.pivot = new Vector2(rect.pivot.x, (h * py + dh) / (h + dh));
-            T17Button modSettingsButton = GameObject.Instantiate(settingsTab.GetChild(0).gameObject).GetComponent<T17Button>();
+            T17Button modSettingsButton = GameObject.Instantiate(settingsTab.Find("GameSettings").gameObject).GetComponent<T17Button>();
             modSettingsButton.gameObject.name = "ModSettings";
             modSettingsButton.transform.SetParent(settingsTab, false);
             modSettingsButton.transform.SetSiblingIndex(3);
+            settingsTab.GetChild(4).localPosition += new Vector3(0, -dh, 0);
 
-            T17Button credits = settingsTab.GetChild(2).GetComponent<T17Button>();
+            T17Button credits = settingsTab.Find("Credits").GetComponent<T17Button>();
             Navigation navigation1 = credits.navigation;
             navigation1.selectOnDown = modSettingsButton;
             credits.navigation = navigation1;
@@ -68,7 +69,7 @@ namespace OC2HardHorde
             navigation2.selectOnUp = credits;
             modSettingsButton.navigation = navigation2;
 
-            T17Text text = modSettingsButton.transform.GetChild(2).GetComponent<T17Text>();
+            T17Text text = modSettingsButton.transform.Find("Text").GetComponent<T17Text>();
             if (Localization.GetLanguage() == SupportedLanguages.Chinese)
             {
                 text.text = "MOD";
@@ -79,7 +80,6 @@ namespace OC2HardHorde
                 text.text = "MODs";
                 text.m_LocalizationTag = "\"MODs\"";
             }
-            settingsTab.GetChild(4).localPosition += new Vector3(0, -dh, 0);
 
             GameObject modSettingsMenuObj = GameObject.Instantiate(gameSettingsMenu.gameObject);
             modSettingsMenuObj.name = "ModOptions";
@@ -87,7 +87,7 @@ namespace OC2HardHorde
             modSettingsMenuObj.transform.SetParent(gameSettingsMenu.transform.parent, false);
             modSettingsMenuObj.transform.SetSiblingIndex(2);
             modSettingsMenu = modSettingsMenuObj.GetComponent<FrontendOptionsMenu>();
-            text = modSettingsMenu.transform.GetChild(0).Find("HeaderBacker").GetChild(0).GetComponent<T17Text>();
+            text = modSettingsMenu.transform.Find("SettingsBody/HeaderBacker/Header").GetComponent<T17Text>();
             if (Localization.GetLanguage() == SupportedLanguages.Chinese)
             {
                 text.text = "MOD设定";
@@ -98,12 +98,12 @@ namespace OC2HardHorde
                 text.text = "MOD SETTINGS";
                 text.m_LocalizationTag = "\"MOD SETTINGS\"";
             }
-            Transform settingsBody = modSettingsMenu.transform.GetChild(0);
+            Transform settingsBody = modSettingsMenu.transform.Find("SettingsBody");
             settingsBody.Find("Cancel").gameObject.Destroy();
             settingsBody.Find("Confirm").gameObject.Destroy();
             settingsBody.Find("ContentConsole").gameObject.Destroy();
             settingsBody.Find("VersionNumber").gameObject.Destroy();
-            Transform content = settingsBody.Find("ContentPC").GetChild(1).GetChild(0);
+            Transform content = settingsBody.Find("ContentPC/Viewport/Content");
             for (int i = content.childCount - 1; i >= 0; i--)
                 content.GetChild(i).gameObject.Destroy();
             rect = settingsBody.Find("ContentPC").GetComponent<RectTransform>();
